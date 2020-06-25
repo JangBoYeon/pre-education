e6e756b02e8fc794f3a4121ac461aaa29360a2fe
JangBoYeon <uko010199@gmail.com>
2020-06-25 17:00:49 +0900

    Answer pre-python_quiz 04, 05, 06

--- quiz/pre_python_04.py
+++ quiz/pre_python_04.py
@@ -10,3 +10,11 @@ print(Triangle(10,20))
 100
 """
 
+
+def Triangle(width, height):   # 가로 width, 높이 height 인 삼각형 함수 선언
+    area = width * height / 2  # 삼각형의 넓이 = 밑변 * 높이 / 2
+    return area  # area 의 값을 리턴
+
+
+print(Triangle(10, 20))     # 삼각형의 밑변과 높이의 값을 받아 넓이를 출력
+
