ae2ec64f9e912cd52a55115984f065dfed3a9861
JangBoYeon <uko010199@gmail.com>
2020-06-25 18:16:19 +0900

    Answer pre-python_quiz 10, 11, 12

--- quiz/algorithm_quiz1.py
+++ quiz/algorithm_quiz1.py
@@ -14,7 +14,7 @@ print(factorial(10))
 def factorial(n):   # 팩토리얼 def 함수 선언
     if n == 1:      # if n이 1 이라면
         return 1    # 1로 리턴
-    return n * factorial(n - 1) # 아니라면 n * 
+    return n * factorial(n - 1)  # 아니라면 n * 팩토리얼 n - 1로 리턴
 
 
 print(factorial(10))
