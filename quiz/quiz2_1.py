9457560bbb2b2731be84c7ec612895c5820946db
JangBoYeon <uko010199@gmail.com>
2020-06-29 13:45:37 +0900

    Answer quiz2_1

--- quiz/quiz2_1.py
+++ quiz/quiz2_1.py
@@ -13,3 +13,23 @@ print(is_palindrome("토마토"))
 False
 True
 """
+
+
+def is_palindrome(word):
+    list_word = list(word)
+    # is_palindrome 메서드에서 word 를 넘겨받아 list(word)로 리스트화 시킨다
+    for i in range(0, len(list_word) // 2):
+        # for 문으로 word 의 절반까지만 검사
+        # /를 사용하면 float 형태가 나오기 때문에 //를 사용
+        if list_word[i] == list_word[len(list_word) - 1 - i]:
+            # [0]과 [-1]마지막 인덱스만 검사
+            # len(list)는 1부터 시작하기 때문에 -1가 마지막 인덱스 임을 뜻한다
+            continue    # 조건을 계속해서 검사
+        else:
+            return False    # 팰린드롬이 아니라면 False 출력
+    return True     # 팰린드롬이라면 True 출력
+
+
+print(is_palindrome("radio"))       # 팰린드롬 검사
+print(is_palindrome("토마토"))       # 팰린드롬 검사
+
